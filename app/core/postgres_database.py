733a47fcb2eccc2010ca064c170a693f8e941d77
digo5ds <diogo.silva5ds@outlook.com>
2025-08-07 18:54:25 -0300

    creating database connection factory

--- app/core/postgres_database.py
+++ app/core/postgres_database.py
@@ -4,7 +4,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+psycopg2://user:password@db:5432/transactions_db"
+from app.core.config import (
+    POSTGRES_DB,
+    POSTGRES_PASSWORD,
+    POSTGRES_PORT,
+    POSTGRES_USER,
+    POSTIGRES_HOST,
+)
+
+DATABASE_URL = (
+    "postgresql+psycopg2://"
+    f"{POSTGRES_USER}:{POSTGRES_PASSWORD}"
+    f"@{POSTIGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+)
 
 engine = create_engine(DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
