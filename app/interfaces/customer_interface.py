1069bd1e9cce2dc6949f56b7cd9ef863a7e91670
digo5ds <diogo.silva5ds@outlook.com>
2025-08-08 22:58:35 -0300

    improvments interfaces

---
+++ app/interfaces/customer_interface.py
@@ -0,0 +1,40 @@
+"""Interface for customer_helper"""
+
+from abc import ABC, abstractmethod
+
+from app.models.customer import Customer
+
+
+class CustomerInterface(ABC):
+    """
+    Abstract base class that defines the interface for a customer object.
+    """
+
+    @abstractmethod
+    def insert(self, customer: Customer):
+        """
+        Saves a customer profile into the database.
+
+        Args:
+            customer_data (PutCustomerRequest): The customer data to be saved.
+
+        Returns:
+            Customer: The saved customer object.
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def get_customer_by_id(self, customer: Customer):
+        """
+        Retrieves a customer by their customer ID.
+
+        Args:
+            customer (Customer): The customer object containing the ID to be retrieved.
+
+        Returns:
+            Customer: The retrieved customer object.
+
+        Raises:
+            SQLAlchemyError: If an error occurs during the database query.
+        """
+        raise NotImplementedError()
