3a42b3cf0f9058a3c4582eb0323b2c96ee65ab66
digo5ds <diogo.silva5ds@outlook.com>
2025-08-10 05:09:58 -0300

    feat: creating migrate script

--- migrate.py
+++ migrate.py
@@ -1,19 +1,60 @@
-import os
+"""
+This module provides a function to migrate the MongoDB database."""
 
-from alembic import command
-from alembic.config import Config
+import alembic
+from app.core.constants import INITIAL_RULES
+from app.core.mongo_database import mongo_connection
+from app.helpers.mongo_helper import MongoHelper
+from app.models.collections.requests_model import RequestLog
+from app.models.collections.rules_model import Rule
+from app.models.collections.user_cache_model import KnowlegedDestinations
 
-from app.core.postgres_database import DATABASE_URL
+
+def migrate_mongo():
+    """
+    Ensures the indexes for the RequestLog and KnowlegedDestinations collections exist.
+
+    This function is intended to be used as a one-time operation to create the indexes
+    for the MongoDB collections. It should be called from the command-line interface.
+    """
+    with mongo_connection():
+        # RequestLog.ensure_indexes()
+        KnowlegedDestinations.ensure_indexes()
+        RequestLog.ensure_indexes()
+        Rule.ensure_indexes
 
 
-def run_migrations(database_url: str):
-    alembic_cfg = Config()
-    alembic_cfg.set_main_option("script_location", "alembic")
-    alembic_cfg.set_main_option("sqlalchemy.url", database_url)
+def migrate_postgres():
+    """
+    Runs the Alembic migrations for the PostgreSQL database.
 
-    command.upgrade(alembic_cfg, "head")
+    This function is intended to be used as a one-time operation to apply the
+    database migrations. It should be called from the command-line interface.
+    """
+    alembic.command.upgrade("alembic.ini", "head")
+
+
+def populate_rules():
+    """
+    Populates the rules collection with the initial rules.
+
+    This function is intended to be used as a one-time operation to populate the
+    rules collection with the initial rules. It should be called from the
+    command-line interface.
+
+    """
+    with mongo_connection():
+        mongo_helper = MongoHelper()
+        for name, conditions in INITIAL_RULES.items():
+            mongo_helper.save(Rule(name=name, conditions=conditions))
+
+    # migrate_postgres()
 
 
 if __name__ == "__main__":
-    db_url = DATABASE_URL
-    run_migrations(db_url)
+    try:
+        migrate_mongo()
+        populate_rules()
+        migrate_postgres()
+    except Exception as e:
+        print(e)
